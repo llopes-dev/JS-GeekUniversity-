9dee95e846c953da5c062ad0c47cf3e7442bfe1b
leandro <leandro.lopes.dev@gmail.com>
2020-05-06 23:04:51 -0300

    instanciando Objetos

---
+++ secao07/aula07.js
@@ -0,0 +1,11 @@
+const curso = {
+    nome:'Curso de Programação',
+    hora: 25,
+    preco: 22.90,
+}
+
+// console.log(curso);
+// console.log(typeof(curso));
+// console.log(Object.keys(curso));
+// console.log(Object.values(curso));
+console.log(Object.entries(curso));
