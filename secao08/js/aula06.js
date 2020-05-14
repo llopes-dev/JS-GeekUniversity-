d75bd63b7ef8cacef034480968d1a5010ff9d57a
Leandro Lopes <leandro@leandro.pro>
2020-05-14 20:13:44 -0300

    Exercicios de Manipulação do DOM

--- secao08/js/aula06.js
+++ secao08/js/aula06.js
@@ -2,7 +2,7 @@ let caixaElement = document.querySelector('.caixa');
 
 caixaElement.style.width = "200px";
 caixaElement.style.height = "200px";
-caixaElement.style.backgroundColor = "green";
+caixaElement.style.backgroundColor = "yellow";
 
 /**
  * As configs de estilos via JS seguem os
