d9d7b2a81eaec41f2f433cea57041ae1a102e70d
LeandroLopes <leandrolopes@e-ig-2171s-Mac-mini.local>
2020-05-13 23:00:38 -0300

    O que é o DOM e Eventos Inline

---
+++ secao08/js/aula02.js
@@ -0,0 +1,14 @@
+let a  = document.querySelector('.link');
+
+// console.log(a.parentNode);
+// console.log(a.parentNode.parentNode);
+
+// console.log(a.parentNode.parentNode.childNodes);
+
+// console.log(a.parentNode.parentNode.firstChild); // head
+
+
+// console.log(a.parentNode.parentNode.lastChild); // Body
+
+// console.log(a.parentNode.parentNode.firstChild.nextSibling.firstChild.firstChild);
+
